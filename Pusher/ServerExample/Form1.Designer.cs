8502d395f5795cc7a03e06eab60bbcb26ef6a1b2
Seth Beaty <callmeippy@gmail.com>
2021-10-18 11:47:09 -0500

    Add files via upload
    Added the server-side files that the game will use to update the score on a webpage, as well as the PDFs of all the code files.

---
+++ Pusher/ServerExample/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PusherTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendEvent = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSendEvent
+            // 
+            this.btnSendEvent.Location = new System.Drawing.Point(169, 56);
+            this.btnSendEvent.Name = "btnSendEvent";
+            this.btnSendEvent.Size = new System.Drawing.Size(75, 23);
+            this.btnSendEvent.TabIndex = 0;
+            this.btnSendEvent.Text = "Send Event";
+            this.btnSendEvent.UseVisualStyleBackColor = true;
+            this.btnSendEvent.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(29, 34);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(53, 13);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Message:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(32, 56);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(100, 20);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSendEvent;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 95);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnSendEvent);
+            this.Name = "Form1";
+            this.Text = "Pusher Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendEvent;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ Pusher/ServerExample/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PusherServer;
+
+namespace PusherTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Pusher can be installed using Nuget with "Install-Package PusherServer -Pre"
+            //Alternatively, You can download it and add it to your project manually from https://github.com/pusher/pusher-dotnet-server
+
+            //Create pusher object using YOUR APP KEY and YOUR SECRET APP KEY
+            var Pusher = new Pusher("App Id", "APP KEY", "SECRET KEY");
+
+            //Trigger an event on some channel with a message
+            var result = Pusher.Trigger("channel1", "test_event", new { message = txtMessage.Text});
+
+            txtMessage.Text = "";
+
+            //result will hold some data about what happened when the event was triggered. StatusCode should just be OK when successful.
+            //MessageBox.Show(result.StatusCode + ":" + result.Body);
+        }
+    }
+}
